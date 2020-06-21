236ff330edabe6a7f9150a7d19746d98027f092c
Mike Yachanin <michael@yachanin.com>
2020-06-21 03:41:38 -0700

    Add script to install programs after fresh arch install

---
+++ install-archlinux-programs.sh
@@ -0,0 +1,25 @@
+# make sure all packages are up-to-date
+sudo pacman -Syy
+
+# base GUI
+sudo pacman -S xf86-video-intel xorg lightdm lightdm-gtk-greeter i3 dmenu xfce4-terminal
+sudo systemctl enable lightdm
+
+# data
+sudo pacman -S nemo syncthing syncthing-gtk
+
+# dev tools
+sudo pacman -S git zsh
+
+# fonts
+sudo pacman -S ttf-dejavu ttf-liberation noto-fonts
+
+# networking
+sudo pacman -S networkmanager network-manager-applet
+sudo systemctl enable NetworkManager
+
+# ops tools
+sudo pacman -S htop openssh tmux vim
+
+# audio
+sudo pacman -S pulseaudio pulseaudio-alsa pavucontrol
